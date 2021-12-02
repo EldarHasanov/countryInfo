74cab9ba19cca61dae15d94c6d6e6120d9894ccb
EldarHasanov <eldargasanovtr91@gmail.com>
2021-12-02 20:08:01 +0200

    Added Moq

---
+++ countryInfo/DLA.Tests/BaseRepositoryUnitTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DLA.Entites;
+using DLA.Repositories.Impl;
+using Microsoft.EntityFrameworkCore;
+
+namespace DLA.Tests
+{
+    class TestLocalityRepository
+        : BaseRepository<Lokality>
+    {
+        public TestLocalityRepository(DbContext context)
+            : base(context)
+        {
+        }
+    }
+}
